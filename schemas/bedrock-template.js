7eca706735c676fb59e9c9b3825637af76977433
David I. Lehn <dlehn@digitalbazaar.com>
2026-04-06 20:53:51 -0400

    Fix schema example.

--- schemas/bedrock-template.js
+++ schemas/bedrock-template.js
@@ -3,12 +3,14 @@
  */
 import {schemas} from '@bedrock/validation';
 
-export const example = {
-  title: 'Example',
-  type: 'object',
-  required: ['id'],
-  additionalProperties: false,
-  properties: {
-    id: schemas.identifier()
-  }
-};
+export function postExample() {
+  return {
+    title: 'Post Example',
+    type: 'object',
+    required: ['id'],
+    additionalProperties: false,
+    properties: {
+      id: schemas.identifier()
+    }
+  };
+}
